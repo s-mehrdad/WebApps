a584fb6614c16bfb1266a479a49d4967c8368ccd
Mehrdad Soleimanimajd <s.mehrdad.47@outlook.com>
2023-05-18 01:13:26 +0330

    nodeJs+Readme+Bugs

---
+++ nodejs/README.md
@@ -0,0 +1,49 @@
+# Introduction 
+Responsive effects in web applications build on back of HTML, CSS and JavaScript and backed from nodeJs.
+
+https://user-images.githubusercontent.com/43612903/234182369-b3c02f50-bc56-4c13-b8b5-981b019b717b.mov
+
+<img href="https://github.com/s-mehrdad" src="https://github.com/s-mehrdad/WebApps/blob/master/CssHtmlJs/effects/screenshots/ScreenShot-2023-04-22-at-10-42-16-PM.png" width="824" height="464" alt="ScreenShot">
+
+<table>
+<tr>
+<td><b>clone:</b></td>
+<td>git clone git@github.com:s-mehrdad/WebApps.git</td>
+</tr>
+</table>
+
+
+# Getting Started
+1.  Installation process:<br/>
+node nodeJs.js
+
+2.  Software dependencies:<br/>
+Dependent on nodeJs modules.
+
+3.  Latest releases:<br/>
+The source code is released.
+
+4.  API references:<br/>
+
+# Build and Test
+Run on nodeJs and test.
+
+
+# Contribute
+I appreciate any contribution to the project.
+
+# References
+<table>
+
+<tr>
+<td><a href="https://www.w3schools.com/">W3Schools</a></td>
+<td><a href="https://freefrontend.com/">Free Fronted</a></td>
+<td><a href="https://developer.mozilla.org/">MDN Web Docs</a></td>
+<td><a href="https://www.codecademy.com/">Code Academy</a></td>
+<td><a href="https://stackoverflow.com/">Stackoverflow</a></td>
+</tr>
+
+</table>
+
+# Copyright Notice
+The literature and all the known and unknown ideas provided within the following learning resource is copyrighted. On the other hand, so far the source code is concerned, most of which is learned and inspired from online references and the addition to them can be used under the license terms provided with the software itself.
---
+++ nodejs/nodeJs.js
@@ -0,0 +1,107 @@
+
+// --------------------------------------------------------------------------------
+/// <summary>
+/// helloNodejs.js
+/// nodeJs
+/// created by Mehrdad Soleimanimajd on 11.05.2023
+/// </summary>
+/// <created>ʆϒʅ, 11.05.2023</created>
+/// <changed>ʆϒʅ, 18.05.2023</changed>
+// --------------------------------------------------------------------------------
+
+
+var http = require('node:http');
+var net = require('net');
+const url = require('url');
+const assertion = require('assert');
+var fs = require('fs');
+var path = require('node:path');
+const dt = require('./dt');
+const hostname = '127.0.0.1';
+var port = 3000;
+const process = require('node:process');
+const input = process.stdin;
+const output = process.stdout;
+const readline = require('readline').createInterface({ input, output });
+
+
+var check = net.createServer();
+check.listen(port, hostname, () => {
+  do {
+    console.log('port ' + port.toString() + ' checked!');
+    port++;
+  } while (check.on('error', (e) => {
+    if (e.code == 'EADDRINUSE') {
+      return 'no';
+    } else {
+      return 'yes';
+      check.close();
+    }
+  }) == 'no');
+}
+);
+
+var server = http.createServer((req, res) => {
+
+  let filePath = 'index.html';
+  filePath = '.' + req.url;
+  if (filePath == './') {
+    filePath = 'index.html';
+  }
+
+  let contentT = 'text/html';
+  switch (path.extname(req.url)) {
+    case '.html':
+      contentT = 'text/html';
+      break;
+    case '.css':
+      contentT = 'text/css';
+      break;
+    case '.js':
+      contentT = 'text/javascript';
+      break;
+    default:
+      break;
+  }
+  console.log(filePath);
+  console.log(path.extname(filePath));
+  let pathN = '';
+  fs.readFile(filePath, (err, data) => {
+    // res.statusCode = 200;
+    res.writeHead(200, { 'Content-Type': contentT });
+    // res.write('<p>current date: ' + dt.dt() + '</p>');
+    // res.write('<p>Hello, world of Node.js!</p>');
+    // res.write('<p>url: ' + req.url + '</p>');
+    res.write(data);
+    // app.use(express.static('public'));
+    // app.use()
+    console.log(err);
+    return res.end();
+  });
+  let a = url.parse(req.url, true);
+  pathN = a.pathname;
+  console.log('Requested url:' + hostname + ':' + port.toString() + pathN.toString() + '/');
+  console.log('url: ' + req.url + '/');
+});
+
+server.once('error', (err) => {
+  if (err.code == "EADDRINUSE") {
+    readline.question('Port is busy! do you want to try' + (port + 1).toString() + ' port? (yes/no)\t> ', (q) => {
+      if (q == 'yes') {
+        server.close();
+        port++;
+        server.listen(port, hostname, () => {
+          console.log('Server running at http://' + hostname.toString() + ':' + port.toString() + '/');
+        });
+      }
+    });
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log('Server running at http://' + hostname.toString() + ':' + port.toString() + '/');
+  let name = '';
+  readline.question('hi?\t> ', name => {
+    console.log('hey ' + name);
+  });
+});
